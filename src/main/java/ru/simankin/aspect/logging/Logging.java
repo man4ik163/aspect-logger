178f6f94cfccf88ecc1a3cc0586e4edb099a509e
Vladimir Simankin <man4ik163@gmail.com>
2020-07-07 17:34:19 +0300

    Change annotation and fix logic

--- src/main/java/ru/simankin/aspect/logging/Logging.java
+++ src/main/java/ru/simankin/aspect/logging/Logging.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ToLog {
+public @interface Logging {
     boolean isDebug() default false;
 }
