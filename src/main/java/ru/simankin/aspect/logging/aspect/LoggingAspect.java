936b5b791530d16a813503c5ce59eb8f0ed90844
Simankin Vladimir <man4ik163@gmail.com>
2020-08-23 19:02:23 +0300

    Feature/logger 4 (#3)
    add error logs

---
+++ src/main/java/ru/simankin/aspect/logging/aspect/LoggingAspect.java
@@ -0,0 +1,121 @@
+package ru.simankin.aspect.logging.aspect;
+
+import java.lang.reflect.Method;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import ru.simankin.aspect.logging.annotation.Logging;
+
+@Slf4j
+@Aspect
+public class LoggingAspect {
+
+    private static final String WHITE_SPACE = " ";
+    private static final String IN = "in";
+    private static final String OUT = "out";
+    private static final String ERROR = "error";
+    private static final String DOT = ".";
+    private static final String COMMA = ",";
+    private static final String LEFT_BRACKET = "[";
+    private static final String RIGHT_BRACKET = "]";
+    private static final String EQUAL_SIGN = "=";
+
+    @Around("@annotation(ru.simankin.aspect.logging.annotation.Logging)")
+    public Object logWithLoggingAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] arguments = joinPoint.getArgs();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        Object[] parameterNames = signature.getParameterNames();
+        String methodName = method.getName();
+        String simpleClassName = method.getDeclaringClass().getSimpleName();
+        boolean isDebug = method.getAnnotation(Logging.class).isDebug();
+
+        String inLog = appendInLog(arguments, parameterNames, methodName, simpleClassName);
+        log(inLog, isDebug);
+
+        Object proceed = new Object();
+        try {
+            proceed = joinPoint.proceed();
+
+            String outLog = appendOutLog(proceed, methodName, simpleClassName);
+            log(outLog, isDebug);
+        } catch (Exception e) {
+            String errorLog = appendErrorLog(methodName, simpleClassName, e);
+            log.error(errorLog);
+        }
+
+        return proceed;
+    }
+
+    private void log(String message, boolean isDebug) {
+        if (isDebug) {
+            log.debug(message);
+        } else {
+            log.info(message);
+        }
+    }
+
+    private String appendInLog(Object[] arguments, Object[] parameterNames, String methodName, String simpleClassName) {
+        StringBuilder resultLog = new StringBuilder();
+        resultLog.append(IN)
+                .append(WHITE_SPACE)
+                .append(simpleClassName)
+                .append(DOT)
+                .append(methodName);
+        int countArguments;
+
+        if (arguments.length == 0 || parameterNames.length == 0 || arguments.length != parameterNames.length) {
+            return resultLog.toString();
+        }
+
+        countArguments = arguments.length;
+
+        resultLog.append(LEFT_BRACKET);
+        for (int i = 0; i < countArguments; i++) {
+            resultLog.append(parameterNames[i])
+                    .append(EQUAL_SIGN)
+                    .append(arguments[i]);
+            if (i < countArguments - 1) {
+                resultLog.append(COMMA)
+                        .append(WHITE_SPACE);
+            }
+        }
+        resultLog.append(RIGHT_BRACKET);
+
+        return resultLog.toString();
+    }
+
+    private String appendOutLog(Object proceed, String methodName, String simpleClassName) {
+        StringBuilder resultLog = new StringBuilder();
+        resultLog.append(OUT)
+                .append(WHITE_SPACE)
+                .append(simpleClassName)
+                .append(DOT)
+                .append(methodName);
+
+        if (proceed == null) {
+            return resultLog.toString();
+        }
+
+        resultLog.append(LEFT_BRACKET)
+                .append(proceed)
+                .append(RIGHT_BRACKET);
+
+        return resultLog.toString();
+    }
+
+    private String appendErrorLog(String methodName, String simpleClassName, Throwable e) {
+        StringBuilder resultLog = new StringBuilder();
+        resultLog.append(ERROR)
+                .append(WHITE_SPACE)
+                .append(simpleClassName)
+                .append(DOT)
+                .append(methodName)
+                .append(WHITE_SPACE)
+                .append(e);
+
+        return resultLog.toString();
+    }
+}
--- src/main/java/ru/simankin/aspect/logging/configuration/LoggingConfiguration.java
+++ src/main/java/ru/simankin/aspect/logging/configuration/LoggingConfiguration.java
@@ -1,7 +1,8 @@
-package ru.simankin.aspect.logging;
+package ru.simankin.aspect.logging.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.simankin.aspect.logging.aspect.LoggingAspect;
 
 @Configuration
 public class LoggingConfiguration {
